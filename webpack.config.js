381a06df6073fe1db83ee2a0250609d7b1581e33
Jucov Alexandr <sania337@gmail.com>
2018-12-13 00:06:47 +0200

    c

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
+		query :{
+				presets:['react','es2015']
+			}
             },
         ],
     },
